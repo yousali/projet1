54957f4533a2b2bb9f246d7b6268a2b81c80cfaf
yousali <aliyousfi@gmail.com>
2014-10-03 00:20:28 -0400

    jeudi 2 Octobre 2014

--- src/projet1/Projet1.java
+++ src/projet1/Projet1.java
@@ -1,4 +1,3 @@
-
 package projet1;
 
 import java.io.FileWriter;
@@ -13,50 +12,152 @@ import net.sf.json.JSONSerializer;
 public class Projet1 {
 
     public static void main(String[] args) throws Exception {
-        String fichierDeclaration =   args[0];
+        String fichierDeclaration = args[0];
+        //A RAJOUTER LE FICHIER DE SORTIE ICI
+        Boolean declarationValide = true;
         ArrayList<String> erreurs = new ArrayList<String>();
-        String categoriesReconnues[] = new String[]{"cours", "atelier", "séminaire", "colloque", "conférence", "lecture dirigée", "présentation", "groupe de discussion", "projet de recherche", "rédaction professionnelle"};
+        ArrayList<String> activitesTraitees = new ArrayList<String>();
+         Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse("2012-04-01");
+            Date date2 = new SimpleDateFormat("yyyy-MM-dd").parse("2014-04-01");
+        Integer nbreHeuresCours = 0;
+        Integer nbreHeuresPresentation = 0;
+        Integer  nbreHeuresGroupeDeDiscussion = 0;
+        Integer  nbreHeuresProjetDeRecherche = 0;
+        Integer nbreHeuresRedaction = 0;
+        
+        
 
         String fichier = FileReader.loadFileIntoString(fichierDeclaration, null);
         JSONObject contenu = (JSONObject) JSONSerializer.toJSON(fichier);
         String numeroDePermis = contenu.getString("numero_de_permis");
         String cycle = contenu.getString("cycle");
-        int heuresTransfereesCylePrecedent = contenu.getInt("heures_transferees_du_cycle_precedent");
-        JSONArray activitesFaites = (JSONArray) contenu.getJSONArray("activites");
+        if (!cycle.equals("2012-2014")) {
 
-        //-------------------------
-        Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse("2012-04-01");
-        Date date2 = new SimpleDateFormat("yyyy-MM-dd").parse("2014-04-01");
-        for (int i = 0; i < activitesFaites.size(); i++) {
+            erreurs.add("Le cyle entré n'est pas supporte");
+            declarationValide = false;
 
-            JSONObject activiteSuivie = activitesFaites.getJSONObject(i);
-            String descriptionActivite = activiteSuivie.getString("description");
-            String categorieActivite = activiteSuivie.getString("categorie");
-            int nombreHeuresActivite = activiteSuivie.getInt("heures");
-            String dateActivite = activiteSuivie.getString("date");
-            Date date = new SimpleDateFormat("yyyy-MM-dd").parse(dateActivite);
-            System.out.println(descriptionActivite);
-            System.out.println(categorieActivite);
-            System.out.println(nombreHeuresActivite);
-            System.out.println("******");
-            System.out.println(date);
-            System.out.println("******");
-            System.out.println("------------******");
-            if (date.compareTo(date1) > 0 && date.compareTo(date2) < 0) {
+            System.out.println(erreurs);
+            System.out.println("#################");
+            System.out.println("Déclaration non valide");
+            System.out.println("#################");
 
-                System.out.println("GOOOOOOOD");
-            } else {
+        } else {
+            int heuresTransfereesCylePrecedent = contenu.getInt("heures_transferees_du_cycle_precedent");
+            
+            JSONArray activitesFaites = (JSONArray) contenu.getJSONArray("activites");
+           
 
-                System.out.println("BAAAAAAAAD");
+            for (int i = 0; i < activitesFaites.size(); i++) {
+
+                JSONObject activiteSuivie = activitesFaites.getJSONObject(i);
+                String descriptionActivite = activiteSuivie.getString("description").trim().toLowerCase();
+                String dateActivite = activiteSuivie.getString("date").trim();
+                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(dateActivite);
+                if (date.compareTo(date1) < 0 || date.compareTo(date2) > 0) {
+
+                    erreurs.add("L'activité \"" + descriptionActivite + "\" n'a pas ete complétée"
+                            + " entre entre le 1er Avril 2012 et le 1er Avril 2014.");
+
+                } else {
+
+                    
+
+                    String categorieActivite = activiteSuivie.getString("categorie").trim().toLowerCase();
+                    
+                    System.out.println(categorieActivite + "" + descriptionActivite + dateActivite);
+                    int nbreHeuresActivite = activiteSuivie.getInt("heures");
+
+                    switch (categorieActivite) {
+                        case "cours":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresCours = nbreHeuresCours + nbreHeuresActivite;}
+                            
+                            
+                            break;
+                        case "atelier":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresCours = nbreHeuresCours + nbreHeuresActivite;}
+                            
+                            break;
+                        case "séminaire":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresCours = nbreHeuresCours + nbreHeuresActivite;}
+                            
+                            break;
+                        case "colloque":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresCours = nbreHeuresCours + nbreHeuresActivite;}
+                            
+                            break;
+                        case "conférence":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresCours = nbreHeuresCours + nbreHeuresActivite;}
+                            
+                            break;
+                        case "lecture dirigée":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresCours = nbreHeuresCours + nbreHeuresActivite;}
+                            
+                            break;
+                        case "présentation":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresPresentation = nbreHeuresPresentation + nbreHeuresActivite;}
+                            
+                            break;
+                        case "groupe de discussion":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresGroupeDeDiscussion = nbreHeuresGroupeDeDiscussion + nbreHeuresActivite;}
+                            
+                            break;
+                        case "projet de recherche":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresProjetDeRecherche = nbreHeuresProjetDeRecherche + nbreHeuresActivite;}
+                            
+                            break;
+                        case "rédaction professionnelle":
+                            activitesTraitees.add("" + categorieActivite + descriptionActivite + dateActivite);
+                            if(nbreHeuresActivite > 0){
+                            nbreHeuresRedaction = nbreHeuresRedaction + nbreHeuresActivite;}
+                            
+                            break;
+                        default:
+                            erreurs.add("La catégorie \""+ categorieActivite + "\" n'est reconnue.");
+                           // System.out.println(categorieActivite+ " n'est pas une activite reconnue.");
+
+                    }
+
+                }
+
+                
+               
+
             }
-            System.out.println("------------******");
+            
+            System.out.print("Nombre heures cours:  ");
+            System.out.println(nbreHeuresCours);
+                 System.out.print("Nombre heures presentation: ");
+                 System.out.println(nbreHeuresPresentation);
+                System.out.print("Nombre heures groupe de discussion:  ");
+                System.out.println(nbreHeuresGroupeDeDiscussion);
+                System.out.print("Nombre heures projet derecherche:   ");
+                System.out.println(nbreHeuresProjetDeRecherche);
+                System.out.print("Nombre heure redaction:   ");
+                System.out.println(nbreHeuresRedaction);
+                
 
         }
-
-        System.out.println(numeroDePermis);
-        System.out.println(cycle);
-        System.out.println(heuresTransfereesCylePrecedent);
-
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+            System.out.println(erreurs);
+            System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            System.out.println(activitesTraitees);
     }
-
 }
